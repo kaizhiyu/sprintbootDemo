49d38c4139ce52a6b31f641744aae8170c96e85f
chenchen45 <chenchen45@guazi.com>
2020-07-31 14:01:11 +0800

    add

---
+++ src/main/java/com/example/springboot_demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springboot_demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Welcome to SpringBoot!!!";
+    }
+
+}
